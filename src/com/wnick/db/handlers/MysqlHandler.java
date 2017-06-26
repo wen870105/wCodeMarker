bd0eee63f913c6baf703c32fa576247b4acb9363
wEn <wsy48420@lv.com>
2017-06-26 17:33:20 +0800

    优化

--- src/com/wnick/db/handlers/MysqlHandler.java
+++ src/com/wnick/db/handlers/MysqlHandler.java
@@ -11,7 +11,29 @@ import com.wnick.db.DBBase;
  * @email wnick123@gmail.com
  */
 public class MysqlHandler extends AbstractHandler{
+    
+    private static final String PRIMARYKEY_SQL="describe %s";
+    
+    private static final String COLUMN_SQL = 
+        "select column_name,data_type,column_comment from "
+        + " information_schema.columns " 
+        + " where table_schema = '%s'"
+        + " and table_name = ? ";
+    
+    private static final String TABLE_SQL = 
+        "select table_name,table_comment " 
+        + " from information_schema.tables "
+        + " where table_schema = '%s'";
+        
 
+    // 数据库名称
+    private String dataBaseName;
+    
+    public MysqlHandler() {
+        this.dataBaseName = getDBname();
+        System.out.println("DBName=" + this.dataBaseName);
+    }
+    
 	@Override
 	/**
 	 * 
@@ -26,26 +48,14 @@ public class MysqlHandler extends AbstractHandler{
 		}}
 		return null;
 	}
-
-	// 数据库名称
-	private String dataBaseName;
 	
-	public MysqlHandler() {
-		this.dataBaseName = ResourceManager.getInstance().getString("jdbc.dbname");
+	
+	private static String getDBname(){
+	    String s = ResourceManager.getInstance().getString("jdbc.url");
+        String s1 = s.substring(0,s.indexOf("?"));
+        return s1.substring(s1.lastIndexOf("/") +1);
 	}
-	private static final String PRIMARYKEY_SQL="describe %s";
 	
-	private static final String COLUMN_SQL = 
-		"select column_name,data_type,column_comment from "
-		+ " information_schema.columns " 
-		+ " where table_schema = '%s'"
-		+ " and table_name = ? ";
-	
-	private static final String TABLE_SQL = 
-		"select table_name,table_comment " 
-		+ " from information_schema.tables "
-		+ " where table_schema = '%s'";
-		
 	
 	@Override
 	String getColumnMetadataSql() {
