cae55a76c4a6e10a5541507f64a60aee1ae8b4dc
wEn <wsy48420@lv.com>
2017-06-26 18:24:37 +0800

    tc cfg

--- src/com/wnick/Main.java
+++ src/com/wnick/Main.java
@@ -4,6 +4,7 @@ import com.wnick.db.handlers.HandlerFacade;
 
 public class Main {
 	public static void main(String[] args) {
+	    System.out.println("提示生成代码是注意包名,当前包名="+ResourceManager.getInstance().getString("package"));
 		long s = System.currentTimeMillis();
 		new HandlerFacade();
 		long ret = System.currentTimeMillis() - s;
--- src/com/wnick/ftl/copyright.ftl
+++ src/com/wnick/ftl/copyright.ftl
@@ -1,5 +1,4 @@
-/*
- * Copyright (c) ${date} ${package} All rights reserved.
- * 本软件源代码版权归----所有,未经许可不得任意复制与传播.
- * ${date} 生成于wCodeMaker wnick123@gmail.com
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2017 All Rights Reserved.
  */
